128135df26a600c1b26cbbbf3f486111749494c6
lih-mr <110060701+lih-mr@users.noreply.github.com>
2024-09-25 22:34:17 -0300

    Update runner.js

--- runner.js
+++ runner.js
@@ -4,7 +4,8 @@ const tesults = require('cypress-tesults-reporter');
 const TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6Ijc1OGQxNmZjLWRjM2ItNDY4OS04MjQ2LThkOTgyNDE3OTZhMC0xNzI3MjI1MDEyNTIzIiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiMTZjMDQ5NDktMzRkYS00NzBkLTg1OWYtZDczMzc3MTk3NDZkIiwidHlwZSI6InQifQ.Wo8CfKOXK9_1Bi9n0v_AdhZNnN83Xq_yAYGE3pzgOrU'
 
 cypress.run({
-  // specs to run here
+  video: true, // ativa a gravação de vídeos
+  spec: 'cypress/e2e/*
 })
 .then((results) => {
   const args = {
